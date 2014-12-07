8b23825ea0073777a7d4342d5efc9f3324f65cb7
jftaylor21 <jftaylor21@gmail.com>
2014-12-07 17:12:10 -0500

    tutorial 103

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+
+    // FixedUpdate happens before physics update
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizontal, 0, vertical);
+
+        rigidbody.AddForce(movement * speed * Time.deltaTime);
+    }
+}
